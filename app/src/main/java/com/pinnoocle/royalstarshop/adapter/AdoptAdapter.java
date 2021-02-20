2dddccdcf05ae54e57bc967b2be9c5067f13d513
ganwanghua <1345895582@qq.com>
2021-02-20 16:49:49 +0800

    gwh

---
+++ app/src/main/java/com/pinnoocle/royalstarshop/adapter/AdoptAdapter.java
@@ -0,0 +1,73 @@
+package com.pinnoocle.royalstarshop.adapter;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.pinnoocle.royalstarshop.R;
+import com.pinnoocle.royalstarshop.widget.GlideCircleTransform;
+import com.pinnoocle.royalstarshop.widget.GlideRoundTransform;
+
+import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
+
+
+/**
+ * @author: xl
+ * @date: 2017/7/19
+ */
+
+public class AdoptAdapter extends RecyclerView.Adapter<AdoptAdapter.ViewHolder> {
+    private LayoutInflater mInflater;
+    private Context context;
+
+    public AdoptAdapter(Context context) {
+        this.context = context;
+        mInflater = LayoutInflater.from(context);
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.item_video_shop, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+    //**********************itemClick************************
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    private OnItemClickListener mOnItemClickListener;
+
+    public void setOnItemClickListener(OnItemClickListener mOnItemClickListener) {
+        this.mOnItemClickListener = mOnItemClickListener;
+    }
+    //**************************************************************
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView iv_image;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            iv_image = itemView.findViewById(R.id.iv_image);
+        }
+    }
+}
--- app/src/main/java/com/pinnoocle/royalstarshop/video/VideoFragment.java
+++ app/src/main/java/com/pinnoocle/royalstarshop/video/VideoFragment.java
@@ -1,11 +1,45 @@
 package com.pinnoocle.royalstarshop.video;
 
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.pinnoocle.royalstarshop.R;
+import com.pinnoocle.royalstarshop.adapter.AdoptAdapter;
 import com.pinnoocle.royalstarshop.common.BaseFragment;
+import com.pinnoocle.royalstarshop.widget.CommItemDecoration;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+
+import butterknife.BindView;
 
 public class VideoFragment extends BaseFragment {
+    @BindView(R.id.rl_title)
+    RelativeLayout rlTitle;
+    @BindView(R.id.ed_search)
+    EditText edSearch;
+    @BindView(R.id.recycleView)
+    RecyclerView recycleView;
+    @BindView(R.id.refresh)
+    SmartRefreshLayout refresh;
+    AdoptAdapter videoAdapter;
     @Override
     protected int LayoutId() {
         return R.layout.fragment_video;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+        recycleView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        recycleView.addItemDecoration(new CommItemDecoration(getContext(), DividerItemDecoration.VERTICAL, getResources().getColor(R.color.transparent), 30));
+        videoAdapter = new AdoptAdapter(getContext());
+        recycleView.setAdapter(videoAdapter);
     }
 }
