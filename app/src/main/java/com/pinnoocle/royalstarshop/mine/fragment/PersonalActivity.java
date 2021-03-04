7b7ec24add0b47c3477cdf9f84549fb9a7f46e6c
ganwanghua <1345895582@qq.com>
2021-03-04 11:36:51 +0800

    ss

---
+++ app/src/main/java/com/pinnoocle/royalstarshop/mine/fragment/PersonalActivity.java
@@ -0,0 +1,74 @@
+package com.pinnoocle.royalstarshop.mine.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.lxj.xpopup.XPopup;
+import com.lxj.xpopupext.listener.TimePickerListener;
+import com.lxj.xpopupext.popup.TimePickerPopup;
+import com.pinnoocle.royalstarshop.R;
+import com.pinnoocle.royalstarshop.common.BaseActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class PersonalActivity extends BaseActivity {
+
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.rl_title)
+    RelativeLayout rlTitle;
+    @BindView(R.id.iv_right)
+    ImageView ivRight;
+    @BindView(R.id.rl_birth)
+    RelativeLayout rlBirth;
+    @BindView(R.id.tv_birth)
+    TextView tvBirth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        initWhite();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.iv_back, R.id.rl_birth})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.rl_birth:
+                showTimerPickerPopup(tvBirth);
+                break;
+        }
+    }
+
+    private void showTimerPickerPopup(TextView textView) {
+        TimePickerPopup popup = new TimePickerPopup(this)
+                .setTimePickerListener(new TimePickerListener() {
+                    @Override
+                    public void onTimeChanged(Date date) {
+
+                    }
+
+                    @Override
+                    public void onTimeConfirm(Date date, View view) {
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                        textView.setText(simpleDateFormat.format(date));
+                    }
+                });
+
+        new XPopup.Builder(this)
+                .asCustom(popup)
+                .show();
+    }
+}
--- app/src/main/java/com/pinnoocle/royalstarshop/mine/fragment/SettingActivity.java
+++ app/src/main/java/com/pinnoocle/royalstarshop/mine/fragment/SettingActivity.java
@@ -1,18 +1,61 @@
 package com.pinnoocle.royalstarshop.mine.fragment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.pinnoocle.royalstarshop.R;
 import com.pinnoocle.royalstarshop.common.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import ch.ielse.view.SwitchView;
+
 public class SettingActivity extends BaseActivity {
+
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.rl_title)
+    RelativeLayout rlTitle;
+    @BindView(R.id.rl_personal)
+    RelativeLayout rlPersonal;
+    @BindView(R.id.iv_right)
+    ImageView ivRight;
+    @BindView(R.id.iv_right1)
+    ImageView ivRight1;
+    @BindView(R.id.iv_right2)
+    ImageView ivRight2;
+    @BindView(R.id.iv_right3)
+    ImageView ivRight3;
+    @BindView(R.id.switch_disturb)
+    SwitchView switchDisturb;
+    @BindView(R.id.tv_quit)
+    TextView tvQuit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         initWhite();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.iv_back, R.id.rl_personal, R.id.tv_quit})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.rl_personal:
+                Intent intent = new Intent(this, PersonalActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.tv_quit:
+                break;
+        }
     }
 }
